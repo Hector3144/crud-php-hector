92aad112a23d45a6e3e0d04449ed4af4fc668093
hector3144 <hector.perezm3144@gmail.com>
2024-10-06 02:44:28 -0500

    Mejora de tabla y responsive para moviles

--- index.php
+++ index.php
@@ -27,8 +27,8 @@ $tipo_documentos = $sqlTiposDocumentos->fetchAll();
         integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <script src="https://cdn.tailwindcss.com"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
-        crossorigin="anonymous"></script>
+        integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous">
+    </script>
     <script src="https://cdn.tailwindcss.com"></script>
     <link rel="stylesheet" href="https://cdn.tailgrids.com/tailgrids-fallback.css" />
     <script defer src="https://unpkg.com/alpinejs@3.x.x/dist/cdn.min.js"></script>
@@ -36,94 +36,71 @@ $tipo_documentos = $sqlTiposDocumentos->fetchAll();
 
 <body>
 
-    <form action="validar.php" method="post">
+    <!-- inicio de tabla ordenadores-->
+    <div class="mb-3 mt-3 d-flex justify-content-start"></div> <button onclick="abrirModal()" class="bg-green-500 text-white px-4 py-2 rounded">Crear Usuario</button></div>
+    <div class="table-responsive d-none d-md-block">
+        <table class="table table-bordered table-hover">
+                <thead class="table-dark">
+                    <tr>
+                        <th>NOMBRE</th>
+                        <th>NUMERO DE DOCUMENTO</th>
+                        <th>TIPO DE DOCUMENTO</th>
+                        <th>ACCIONES</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php
+                     foreach ($usuarios as $usuario) {
+                    ?>
+                    <tr>
+                        <td><?= $usuario['nombre'] ?></td>
+                        <td><?= $usuario['numero_documento'] ?></td>
+                        <td><?= $usuario['glosa'] ?></td>
+                        <td>
+                            <button onclick='abrirModal(<?= json_encode($usuario) ?>)' class="btn btn-primary btn-sm">EDITAR</button>
+                            <button onclick='abrirModalEliminar(<?= json_encode($usuario) ?>)' class="btn btn-danger btn-sm">ELIMINAR</button>
+                        </td>
+                    </tr>
 
-        <label for="">Tipos de Documentos</label>
-        <select name="tipo_documento" id="tipo_documento">
-            <option value="0">Seleccione un tipo de documento</option>
-            <?php
-            foreach ($usuarios as $usuario) {
-                ?>
-                <option value=""><?= $usuario['glosa'] ?></option>
+                    </tr>
+                    <?php
+                    }?>
+                </tbody>
+        </table>
+    </div>
+    <!-- fin de tabla ordenadores  -->
+
+
+        <!-- Tabla para moviles-->
+        <div class="table-responsive d-md-none">
+            <table class="table table-bordered table-hover">
+                <thead class="table-dark">
+                    <tr>
+                        <th>DETALLES</th>
+                        <th>ACCIONES</th>
+                    </tr>
+                </thead>
+                <tbody>
                 <?php
-            }
-            ?>
-        </select>
-
-        <button type="submit">Validar</button>
-    </form>
-
-    <!-- inicio de tabla -->
-    <section class="bg-white py-20 lg:py-[120px]">
-        <div class="container">
-            <div class="flex flex-wrap -mx-4">
-                <div class="w-full px-4">
-                    <div class="max-w-full overflow-x-auto">
-                        <div class="flex justify-end mb-4">
-                            <button onclick="abrirModal()" class="bg-green-500 text-white px-4 py-2 rounded">Crear
-                                Usuario</button>
-                        </div>
-                        <div class="table-responsive">
-
-                            <table class="table-auto w-full">
-                            <thead class="thead-dark text-center">
-                                <thead>
-                                    <tr class="bg-primary text-center">
-                                        <th
-                                            class="w-1/6 min-w-[160px] text-lg font-semibold text-white py-4 lg:py-7 px-3 lg:px-4 border-r border-transparent">
-                                            Nombre
-                                        </th>
-                                        <th
-                                            class="w-1/6 min-w-[160px] text-lg font-semibold text-white py-4 lg:py-7 px-3 lg:px-4 border-r border-transparent">
-                                            Tipo de Documento
-                                        </th>
-                                        <th
-                                            class="w-1/6 min-w-[160px] text-lg font-semibold text-white py-4 lg:py-7 px-3 lg:px-4 border-r border-transparent">
-                                            Número de Documento
-                                        </th>
-                                        <th
-                                            class="w-1/6 min-w-[160px] text-lg font-semibold text-white py-4 lg:py-7 px-3 lg:px-4 border-r border-transparent">
-                                            Acciones
-                                        </th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    <?php
-                                    foreach ($usuarios as $usuario) {
-                                        ?>
-                                        <tr>
-                                            <td
-                                                class="text-center text-dark font-medium text-base py-5 px-2 bg-[#F3F6FF] border-b border-[#E8E8E8]">
-                                                <?= $usuario['nombre'] ?>
-                                            </td>
-                                            <td
-                                                class="text-center text-dark font-medium text-base py-5 px-2 bg-[#F3F6FF] border-b border-[#E8E8E8]">
-                                                <?= $usuario['glosa'] ?>
-                                            </td>
-                                            <td
-                                                class="text-center text-dark font-medium text-base py-5 px-2 bg-[#F3F6FF] border-b border-[#E8E8E8]">
-                                                <?= $usuario['numero_documento'] ?>
-                                            </td>
-                                            <td
-                                                class="text-center text-dark font-medium text-base py-5 px-2 bg-[#F3F6FF] border-b border-[#E8E8E8]">
-                                                <button onclick='abrirModal(<?= json_encode($usuario) ?>)'
-                                                    class="bg-blue-500 text-white px-4 py-2 rounded">Editar</button>
-                                                <button onclick='abrirModalEliminar(<?= json_encode($usuario) ?>)'
-                                                    class="bg-red-500 text-white px-4 py-2 rounded">Borrar</button>
-                                            </td>
-                                        </tr>
-                                        <?php
-                                    }
-                                    ?>
-                                </tbody>
-                            </table>
-                        </div>
-                    </div>
-                </div>
-            </div>
+                    foreach ($usuarios as $usuario) {
+                ?>
+                    <tr>
+                        <td>
+                            <strong>Nombre:</strong> <?= $usuario['nombre'] ?><br>
+                            <strong>Numero de Documento:</strong> <?= $usuario['numero_documento'] ?><br>
+                            <strong>Tipo de Documento:</strong> <?= $usuario['glosa'] ?></td>
+                        <td>
+                            <button onclick='abrirModal(<?= json_encode($usuario) ?>)' class="btn btn-primary btn-sm">EDITAR</button>
+                            <button onclick='abrirModalEliminar(<?= json_encode($usuario) ?>)' class="btn btn-danger btn-sm">ELIMINAR</button>
+                        </td>
+                    </tr>
+                    <?php
+                    }?>
+                </tbody>
+            </table>
         </div>
-    </section>
-    <!-- fin de tabla  -->
+    </div>
+    <!-- fin tabla moviles -->
     <!-- modal de difuminado oscuro en la parte de atrás -->
     <div id="difuminado" class="hidden fixed inset-0 bg-black bg-opacity-50"></div>
 
@@ -147,9 +124,9 @@ $tipo_documentos = $sqlTiposDocumentos->fetchAll();
                             <?php
                             foreach ($tipo_documentos as $tipo_documento) {
                                 ?>
-                                <option value="<?= $tipo_documento['id_tipo_documento'] ?>"><?= $tipo_documento['glosa'] ?>
-                                </option>
-                                <?php
+                            <option value="<?= $tipo_documento['id_tipo_documento'] ?>"><?= $tipo_documento['glosa'] ?>
+                            </option>
+                            <?php
                             }
                             ?>
                         </select>
